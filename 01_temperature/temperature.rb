0101f99a07f636b147905c3115c5ff6ce7ff3f3f
Naula <naulagratiant@gmail.com>
2017-10-10 16:32:33 +0200

    fichiers bossés

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,13 @@
 #write your code here
+def ftoc(fahrenheit)
+
+return celsius= 0 if fahrenheit == 32 
+
+else (fahrenheit-32)* 5/9
+end
+
+
+def ctof(celsius)
+
+(celsius*9/5)+32
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,21 @@
 #write your code here
+def add (number1 , number2)
+
+add = number1 +number2
+
+
+end
+
+def subtract(number1,number2)
+
+subtract = number1 - number2
+
+end
+
+
+def sum(array)
+
+	array.sum 
+
+
+end
